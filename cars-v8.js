419e4188ce3c3090315b892962a5064e5773264c
Carlie Duperval <carlierichell@gmail.com>
2022-02-26 14:23:41 -0500

    Crate, Update, delete, firestore

---
+++ cars-v8.js
@@ -0,0 +1,73 @@
+import {initializeApp, cert, getApps } from "firebase-admin/app";
+import {getFirestore } from "firebase-admin/firestore";
+import credentials from "./credentials-v8.js";
+
+ const db = () => {
+
+   if (!getApps().length) {
+     const app = initializeApp({
+       credential: cert(credentials),
+     });
+   }
+   return getFirestore();
+
+ };
+
+ const carCollection = db().collection('cars')
+
+
+ const getAllcars = async () => {
+     const result = await carCollection.get()
+
+const cars = result.docs.map(car => {
+
+    return {id : car.id, ...car.data()}
+})  
+console.log(...cars)
+return cars
+
+}
+
+// create car
+
+const createCar = async (car) => {
+const result = await carCollection.add(car)
+console.log(result)
+
+
+}
+
+// create
+// Car({
+//     make: "Toyota",
+//     modele:"Hilux",
+//     year:"2018"
+// })
+
+// delete 
+
+const deleteCar = async (carId)  => {
+    const result = await carCollection.doc(carId).delete()
+    console.log()
+}
+deleteCar("JUpj2ecGp0htZLC7ok2Y")
+
+
+
+
+
+
+//update 
+
+const updateCarModel = async (carId, model) => {
+     const result = await carCollection.doc(carId).update({
+         model: model
+     })
+
+     console.log(result)
+
+ }
+
+//updateCarModel("TMFm7fEEWJPLoWtmoGiH", "charger")
+
+getAllcars()
